2681e028b79d69f87fb91700d046d05ff50b46c8
Gabriel Reis <tharazo101@gmail.com>
2022-01-21 10:38:01 -0300

    Service layer, component registration

--- src/main/java/com/zyontec/course/resources/UserResource.java
+++ src/main/java/com/zyontec/course/resources/UserResource.java
@@ -1,21 +1,35 @@
 package com.zyontec.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.zyontec.course.entities.User;
+import com.zyontec.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Gabriel", "gabriel@gmail.com", "929943239", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 	
 }
